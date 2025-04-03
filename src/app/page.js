d26ae7529142d8458c7667625c2b1177c810b80e
dhruvtank220 <66196954+dhruvtank220@users.noreply.github.com>
2025-04-04 02:25:06 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@ export default function Home() {
   return (
       <main className="bg-black text-white flex flex-col items-center justify-center min-h-screen text-center">
           {/* Logo */}
-          <img src="/logo.png" alt="DJ Dhruv Logo" className="w-48 mb-4" />
+          <img src="/logo.png" alt="DJ Dhruv Logo" className="w-32 h-auto mx-auto" />
+
 
           {/* Heading */}
           <h1 className="text-4xl font-bold">🔥 Welcome to DJ Dhruv! 🎧</h1>
